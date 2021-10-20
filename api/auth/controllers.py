e44f2b6af91d23991a808423bbb57a20fc70fe25
Gideon Yeboah Asante <asanteg36@gmail.com>
2021-10-20 18:12:58 +0000

    Add a method to create a new user and another method test protected route

--- api/auth/controllers.py
+++ api/auth/controllers.py
@@ -10,7 +10,7 @@ def hello():
     return {"message":"Auth blueprint is working"}, 200
 
 @auth.route("/token", methods=["POST"])
-def create_token():
+def auth_create_new_token():
     data = request.json
 
     # Checks whether a user with the given username exists
@@ -22,7 +22,38 @@ def create_token():
             return {"token":access_token, "user_id":user.public_id}, 200
         return {"message": "Invalid credentials"}, 401
 
+@auth.route("/register", methods=["POST"])
+def create_new_user_account():
+    data = request.json
+
+    user_type = data["user_type"]
+
+    email_exists = User.find_by_email(data["email"])
+
+    if email_exists:
+        return {"message": "A user with this email already exists"}, 401
+
+    user = User(username=data["username"],name=data["name"],email=data["email"])
+    user.password = data["password"]
+
+    # Assign the user type to the temporary instance created
+    if user_type == "admin":
+        user.is_admin = True
+    elif user_type == "super_user":
+        user.is_super_user = True
+    elif user_type == "auditor":
+        user.is_auditor = True
+
+    db.session.add(user)
+    db.session.commit()
+
+    return {"message": "User created successfully", "user": User.find_by_email(data)}, 200
+
 @auth.route("/protected")
 @jwt_required()
 def protected():
-    return {"message": "Protected route"}
+    users = User.query.all()
+    user_list = []
+    for user in users:
+        user_list.append(user.name)
+    return {"message": "Protected route", "users": user_list}
