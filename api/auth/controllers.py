ed7b2e9c12988ec81703c69da9b10b1a6382f0b8
Gideon Yeboah Asante <asanteg36@gmail.com>
2021-10-27 01:40:47 +0000

    Modify code for auth controllers and models

--- api/auth/controllers.py
+++ api/auth/controllers.py
@@ -1,14 +1,21 @@
 from flask import Blueprint, request
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, get_current_user
+from flask_jwt_extended import (
+    create_access_token,
+    jwt_required,
+    get_jwt_identity,
+    get_current_user,
+)
 from flask_jwt_extended.utils import get_jwt
 from api import db
 from api.auth.models import SchoolUser, SuperUser, User
 
-auth = Blueprint("auth", __name__,url_prefix="/api/auth")
+auth = Blueprint("auth", __name__, url_prefix="/api/auth")
+
 
 @auth.route("/hello", methods=["GET"])
 def hello():
-    return {"message":"Auth blueprint is working"}, 200
+    return {"message": "Auth blueprint is working"}, 200
+
 
 @auth.route("/token", methods=["POST"])
 def auth_create_new_token():
@@ -27,8 +34,9 @@ def auth_create_new_token():
         password_correct = user.check_password(data["password"])
         if password_correct:
             access_token = create_access_token(identity=user.public_id)
-            return {"token":access_token, "user_id":user.public_id}, 200
+            return {"token": access_token, "user_id": user.public_id}, 200
         return {"message": "Username or password is invalid"}, 401
+
 
 @auth.route("/register", methods=["POST"])
 def auth_create_new_user_account():
@@ -41,41 +49,72 @@ def auth_create_new_user_account():
     if email_exists:
         return {"message": "A user with this email already exists"}, 401
 
-
     # Assign the user type to the temporary instance created
     if user_type == "super_user":
-        super_user = SuperUser(username=data["username"],name=data["name"],email=data["email"])
+        super_user = SuperUser(
+            username=data["username"], name=data["name"], email=data["email"]
+        )
         super_user.password = data["password"]
         db.session.add(super_user)
         db.session.commit()
     elif user_type == "admin":
-        school_user = SchoolUser(username=data["username"],name=data["name"],email=data["email"], is_admin=True, school_id=data["school_id"])
+        school_user = SchoolUser(
+            username=data["username"],
+            name=data["name"],
+            email=data["email"],
+            role=user_type,
+            school_id=data["school_id"],
+        )
         school_user.password = data["password"]
         db.session.add(school_user)
         db.session.commit()
     elif user_type == "auditor":
-        school_user = SchoolUser(username=data["username"],name=data["name"],email=data["email"], is_auditor=True, school_id=data["school_id"])
+        school_user = SchoolUser(
+            username=data["username"],
+            name=data["name"],
+            email=data["email"],
+            role=user_type,
+            school_id=data["school_id"],
+        )
         school_user.password = data["password"]
         db.session.add(school_user)
         db.session.commit()
     elif user_type == "teacher":
-        school_user = SchoolUser(username=data["username"],name=data["name"],email=data["email"], is_teacher=True, school_id=data["school_id"])
+        school_user = SchoolUser(
+            username=data["username"],
+            name=data["name"],
+            email=data["email"],
+            role=user_type,
+            school_id=data["school_id"],
+        )
         school_user.password = data["password"]
         db.session.add(school_user)
         db.session.commit()
     elif user_type == "owner":
-        school_user = SchoolUser(username=data["username"],name=data["name"],email=data["email"], is_owner=True, school_id=data["school_id"])
+        school_user = SchoolUser(
+            username=data["username"],
+            name=data["name"],
+            email=data["email"],
+            role=user_type,
+            school_id=data["school_id"],
+        )
         school_user.password = data["password"]
         db.session.add(school_user)
         db.session.commit()
 
-    return {"message": "User created successfully", "user": User.find_by_email(data["email"]).name}, 200
+    return {
+        "message": "User created successfully",
+        "user": User.find_by_email(data["email"]).name,
+    }, 200
+
 
 @auth.route("/protected")
 @jwt_required()
 def protected():
     if not SuperUser.user_is_super_user(public_id=get_jwt_identity()):
-        return {"message":"User does not have the right priveleges to perform specified actions"}
+        return {
+            "message": "User does not have the right priveleges to perform specified actions"
+        }
     users = User.query.all()
     user_list = []
     for user in users:
--- api/auth/models.py
+++ api/auth/models.py
@@ -2,24 +2,24 @@ from uuid import uuid4
 from api import db
 from werkzeug.security import check_password_hash, generate_password_hash
 
+
 class User(db.Model):
     """User Model
 
     Args:
         db ([type]): [description]
     """
+
     __tablename__ = "ssms_user"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
-    username =db.Column(db.String(20), nullable=False, unique=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password_hash = db.Column(db.String(120), nullable=False)
     public_id = db.Column(db.String(36), unique=True, default=lambda: str(uuid4()))
 
     def __repr__(self) -> str:
-        return (
-            f"<User {self.email}, public_id={self.public_id}>"
-        )
+        return f"<User {self.email}, public_id={self.public_id}>"
 
     @property
     def password(self):
@@ -46,18 +46,61 @@ class User(db.Model):
 
     @classmethod
     def check_user_credentials(cls, username, password):
-        return cls.query.filter_by(username=username,password=password)
+        return cls.query.filter_by(username=username, password=password)
+
 
 class SuperUser(User):
     __tablename__ = "ssms_superuser"
-    id = db.Column(db.Integer, db.ForeignKey("ssms_user.id") ,primary_key=True, autoincrement=True)
-    is_super_user = db.Column(db.Boolean, default=True)
+    id = db.Column(
+        db.Integer, db.ForeignKey("ssms_user.id"), primary_key=True, autoincrement=True
+    )
+    role = db.Column(db.String(10), default="super_user", nullable=False)
+
+    @classmethod
+    def user_is_super_user(cls, public_id):
+        return (
+            True
+            if cls.query.filter_by(public_id=public_id, role="super_user").first()
+            else False
+        )
+
 
 class SchoolUser(User):
     __tablename__ = "ssms_schooluser"
-    id = db.Column(db.Integer, db.ForeignKey("ssms_user.id") ,primary_key=True, autoincrement=True)
-    school_id = db.Column(db.Integer, db.ForeignKey('ssms_school.id'), nullable=False)
-    is_owner = db.Column(db.Boolean, default=False)
-    is_admin = db.Column(db.Boolean, default=False)
-    is_auditor = db.Column(db.Boolean, default=False)
-    is_teacher = db.Column(db.Boolean, default=False)
+    id = db.Column(
+        db.Integer, db.ForeignKey("ssms_user.id"), primary_key=True, autoincrement=True
+    )
+    school_id = db.Column(db.Integer, db.ForeignKey("ssms_school.id"), nullable=False)
+    role = db.Column(db.String(10), nullable=False)
+
+    @classmethod
+    def user_is_owner(cls, public_id):
+        return (
+            True
+            if cls.query.filter_by(public_id=public_id, role="owner").first()
+            else False
+        )
+
+    @classmethod
+    def user_is_admin(cls, public_id):
+        return (
+            True
+            if cls.query.filter_by(public_id=public_id, role="admin").first()
+            else False
+        )
+
+    @classmethod
+    def user_is_auditor(cls, public_id):
+        return (
+            True
+            if cls.query.filter_by(public_id=public_id, role="auditor").first()
+            else False
+        )
+
+    @classmethod
+    def user_is_teacher(cls, public_id):
+        return (
+            True
+            if cls.query.filter_by(public_id=public_id, role="teacher").first()
+            else False
+        )
