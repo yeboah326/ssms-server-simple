601e16659dc9aed5803171f1fbcca5be0b8f85ef
Gideon Yeboah Asante <asanteg36@gmail.com>
2021-11-01 11:36:30 +0000

    Change the database being used for production

--- api/config.py
+++ api/config.py
@@ -2,26 +2,35 @@
 import os
 
 DATABASE_TEST = os.getenv("DATABASE_TEST_URL")
-DATABASE_DEV = os.getenv("DATABASE_DEV_URL")
+DATABASE_DEV = os.getenv("DATABASE_URL")
 DATABASE_PROD = os.getenv("DATABASE_PROD_URL")
+
 
 class Config:
     """Base Configuration"""
+
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
 class TestingConfig(Config):
     """Testing Configuration"""
+
     TESTING = True
     SQLALCHEMY_DATABASE_URI = DATABASE_TEST
 
+
 class DevelopmentConfig(Config):
     """Development Configuration"""
+
     SQLALCHEMY_DATABASE_URI = DATABASE_DEV
+
 
 class ProductionConfig(Config):
     """Production Configuration"""
+
     SQLALCHEMY_DATABASE_URI = DATABASE_PROD
+
 
 ENV_CONFIG_DICT = dict(
     development=DevelopmentConfig, testing=TestingConfig, production=ProductionConfig
