396c0638e0bf54a133293b056678ec16e39ef4e2
Gideon Yeboah Asante <asanteg36@gmail.com>
2021-11-01 11:36:50 +0000

    Add cli command for creating all tables in the database

--- manage.py
+++ manage.py
@@ -19,5 +19,12 @@ def create_db():
     db.session.commit()
 
 
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
 if __name__ == "__main__":
     cli()
