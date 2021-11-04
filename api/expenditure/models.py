2c3887041317962e7e2003d443989cf4ef98bd36
Gideon Yeboah Asante <asanteg36@gmail.com>
2021-11-04 00:38:20 +0000

    Add date_created field to the Expenditure model

--- api/expenditure/models.py
+++ api/expenditure/models.py
@@ -1,3 +1,4 @@
+import datetime
 from api import db
 from dataclasses import dataclass
 
@@ -7,12 +8,14 @@ class Expenditure(db.Model):
     id: int
     description: str
     amount: float
+    date_created: datetime.date
     academic_year_id: int
 
     __tablename__ = "ssms_expenditure"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     description = db.Column(db.String, nullable=False)
     amount = db.Column(db.Float, nullable=False)
+    date_created = db.Column(db.Date(), default=datetime.date.today(), nullable=True)
     academic_year_id = db.Column(
         db.Integer, db.ForeignKey("ssms_academic_year.id"), nullable=False
     )
